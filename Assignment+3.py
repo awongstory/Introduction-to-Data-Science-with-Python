322324f038edf8209109dee24c28afd56f0329cf
Piyush Sambhi <46954957+sambhipiyush@users.noreply.github.com>
2020-05-10 04:53:54 +0530

    Create load_df_to_memory() and change in two function
    Create load_df_to_memory() and changes in answer_one and answer_two functions

--- Assignment+3.py
+++ Assignment+3.py
@@ -58,8 +58,7 @@
 # *This function should return a DataFrame with 20 columns and 15 entries.*
 
 # In[1]:
-
-def answer_one():
+def load_df_to_memory():
     import pandas as pd
     import numpy as np
     energy = pd.read_excel('Energy Indicators.xls', skiprows=17, skip_footer= 38)
@@ -80,7 +79,10 @@ def answer_one():
     df = pd.merge(ScimEn, energy, how='inner',left_on='Country', right_on='Country')
     alldf = pd.merge(df,GDP, how='inner', left_on='Country', right_on='Country')
     alldf = alldf.set_index('Country')
-    return alldf[:15]
+    return alldf
+
+def answer_one():
+    return load_df_to_memory()[:15]
 
 answer_one()
 
@@ -100,29 +102,7 @@ get_ipython().run_cell_magic('HTML', '', '<svg width="800" height="300">\n  <cir
 # In[3]:
 
 def answer_two():
-    import pandas as pd
-    import numpy as np
-    energy = pd.read_excel('Energy Indicators.xls', skiprows=17, skip_footer= 38)
-    energy = energy[['Unnamed: 1', 'Petajoules', 'Gigajoules', '%']]
-    energy.columns = ['Country', 'Energy Supply', 'Energy Supply per Capita', '% Renewable']
-    energy['Energy Supply'] = energy['Energy Supply'] * 1000000
-    energy[['Energy Supply', 'Energy Supply per Capita', '% Renewable']]= energy[['Energy Supply', 'Energy Supply per Capita', '% Renewable']].replace('...', np.NaN)
-    energy['Country'] = energy['Country'].replace({'Republic of Korea' : 'South Korea', 'United States of America': 'United States', 'United Kingdom of Great Britain and Northern Ireland' : 'United Kingdom', 'China, Hong Kong Special Administrative Region' : 'Hong Kong', 'Iran (Islamic Republic of)':'Iran'})
-    energy['Country'].str.replace(r" \(.*\)","")
-    
-    GDP = pd.read_csv('world_bank.csv', skiprows = 4)
-    GDP['Country Name'] = GDP['Country Name'].replace({'Korea, Rep.': 'South Korea', 'Iran, Islamic Rep.': 'Iran', 'Hong Kong SAR, China' : 'Hong Kong'})    
-    GDP = GDP[['Country Name', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015']]
-    GDP.columns = ['Country', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015']
-    
-    ScimEn = pd.read_excel('scimagojr-3.xlsx')
-    
-    df = pd.merge(ScimEn, energy, how='inner',left_on='Country', right_on='Country')
-    alldf = pd.merge(df,GDP, how='inner', left_on='Country', right_on='Country')
-    alldf = alldf.set_index('Country')
-    answer_one = alldf[:15]
-    answer_two = alldf.shape[0] - answer_one.shape[0]
-    return answer_two
+    return load_df_to_memory().shape[0] - answer_one().shape[0]
 
 answer_two()
 
