ff81d5b124d048d3d170ad6118f42d2354a3c8be
Alicia <9159818+awongstory@users.noreply.github.com>
2022-02-11 04:38:46 -0500

    Update Assignment+3.py

--- Assignment+3.py
+++ Assignment+3.py
@@ -2,7 +2,7 @@
 
 # ---
 # 
-# _You are currently looking at **version 1.5** of this notebook. To download notebooks and datafiles, as well as get help on Jupyter notebooks in the Coursera platform, visit the [Jupyter Notebook FAQ](https://www.coursera.org/learn/python-data-analysis/resources/0dhYG) course resource._ Added softwrap to enhance readability!
+# _You are currently looking at **version 1.5** of this notebook. To download notebooks and datafiles, as well as get help on Jupyter notebooks in the Coursera platform, visit the [Jupyter Notebook FAQ](https://www.coursera.org/learn/python-data-analysis/resources/0dhYG) course resource._
 # 
 # ---
 
